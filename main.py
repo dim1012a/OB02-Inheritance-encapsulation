372e5281d69c91e2ea6f0a762ff623bd618b8371
dim1012a <dim1012a@gmail.com>
2024-06-03 16:49:33 +0300

    main commit

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-class User:
-    def __init__(selfself, id, name, user=True):
-        self.id = id
-        self.name = name
-        self.user = user
+class User():
+    def __init__(self, user_id, name):
+        self._user_id = user_id
+        self._name = name
+        self._level = "user"
 
+    def get_user_id(self):
+        return self._user_id
 
+    def get_name(self):
+        return self._name
+
+    def get_level(self):
+        return self._level
+
+    def set_name(self, name):
+        self._name = name
 
 
 class Admin(User):
-    def __init__(self, id, name, user, admin=True):
-        super().__init__(id, name, user)
-        self.admin = admin
+    def __init__(self, user_id, name):
+        super().__init__(user_id, name)
+        self._level = "admin"
+
+    def add_user(self, user_list, user):
+        user_list.append(user)
+        print(f"Пользователь успешно добавлен в список")
+        print(user_list)
+
+    def remove_user(self, user_list, user):
+        user_list.remove(user)
+        print(f"Пользователь успешно выкинут из списка")
+        print(user_list)
+
+users = []
+
+admin = Admin("a1", "Gosha")
+user1 = User('u1', "Stepa")
+
+print(user1.get_name())
+
+admin.add_user(users, user1)
